35512a39ae41a267fd0fc76eca47a1242ba71fe5
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-03-27 18:24:58 +0100

    Add ky package and import in test.ts

--- apps/builder/src/pages/api/test.ts
+++ apps/builder/src/pages/api/test.ts
@@ -1,9 +1,10 @@
+import ky from 'ky'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await new Promise((resolve) => setTimeout(resolve, 11000))
+  await ky.get('https://google.com')
   res.status(200).json({ name: 'John Doe' })
 }
